71fb2eb0ee4dddb6d0c446cf6a1fb4fe387fdc2a
bakerston <czhang6@g.clemson.edu>
2021-09-06 16:29:27 -0400

    add express

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component } from "@angular/core";
+import { NgForm} from "@angular/forms";
+// import { Post } from '../post.model';
+import { PostsService } from "../posts.service";
 
 @Component({
     selector: 'app-post-create',
@@ -8,13 +11,19 @@ import { Component, EventEmitter, Output } from "@angular/core";
 export class PostCreateComponent {
     enteredTitle = '';
     enteredContent = '';
-    @Output() postCreated = new EventEmitter();
+    //@Output() postCreated = new EventEmitter<Post>();
     
-    onAddPost() {
-        const post = {
-            title: this.enteredTitle, 
-            content: this.enteredContent
-        };
-        this.postCreated.emit(post);
+    constructor(public postsService: PostsService){};
+
+    onAddPost(form: NgForm) {
+        if (form.invalid){
+            return;
+        }
+        // const post: Post = {
+        //     title: form.value.title, 
+        //     content: form.value.content
+        // };
+        this.postsService.addPost(form.value.title, form.value.content);
+        form.resetForm();
     }
 }
