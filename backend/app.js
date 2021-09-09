aae3bdcc918afae5d50bd6362800d9893faa3364
bakerston <czhang6@g.clemson.edu>
2021-09-09 00:12:40 -0400

    Added Mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://CHI:ZZqsqgAfs8uXySH5@clustermean.2cwwu.mongodb.net/node-angular?retryWrites=true&w=majority").then(() => {
+    console.log('connected!')
+})
+.catch(() => {
+    console.log('connected failed!')
+});
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -15,28 +26,34 @@ app.use((req, res, next) => {
     next();
 });
 
+
 app.post("/api/posts", (req, res, next) => {
-    const post = req.body;
-    console.log(post);
-    res.status(201).json({
-        message: 'Post added successfully'
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+    post.save().then(createdPost => {
+        res.status(201).json({
+            message: "Post added successfully",
+            postId: createdPost._id
+        });
+    });
+});
+//ZZqsqgAfs8uXySH5
+//66.231.159.104
+app.get('/api/posts', (req, res, next) => {
+    Post.find().then(documents => {
+        res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+        });
     });
 });
 
-//ZZqsqgAfs8uXySH5
-//66.231.159.104
-app.use('/api/posts', (req, res, next) => {
-    const posts = [
-        {id: 'bokerstan', 
-        title: "first", 
-        content:'coming from server'},
-        {id: 'bokerstan2', 
-        title: "second", 
-        content:'coming from server2'}
-    ];
-    return res.status(200).json({
-        message: 'Posts fetched successfully!',
-        posts: posts
+app.delete("/api/posts/:id", (req, res, next) => {
+    Post.deleteOne({_id: req.params.id}).then(result => {
+        console.log(result);
+        res.status(200).json({message: "Post deleted!"});
     });
 });
 
